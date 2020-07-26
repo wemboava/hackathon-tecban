ea7954fefe36e7ab7de208bc3faca880d4a4fea1
willian emboava <w.emboava.o@gmail.com>
2020-07-26 19:42:45 -0300

    styles adjustments

--- components/HomeLayout/styles.ts
+++ components/HomeLayout/styles.ts
@@ -5,4 +5,5 @@ export const Background = styled.ScrollView``;
 
 export const Container = styled.View`
   background-color: ${mainColor};
+  padding: 30px 0 10px 0;
 `;
--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -34,6 +34,7 @@ export default function BottomTabNavigator() {
           color: Colors[colorScheme].text,
         },
       }}
+      
     >
       <BottomTab.Screen
         name="Home"
